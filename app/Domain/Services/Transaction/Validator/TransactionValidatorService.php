a6f8e5d9a12966912519b6ab26ae0c9435abfbd3
Fabio Andrei Lorentz <tifabio@gmail.com>
2022-09-09 14:27:53 -0300

    Insufficient Balance Validation

--- app/Domain/Services/Transaction/Validator/TransactionValidatorService.php
+++ app/Domain/Services/Transaction/Validator/TransactionValidatorService.php
@@ -2,21 +2,31 @@
 
 namespace App\Domain\Services\Transaction\Validator;
 
-use App\Domain\Entities\Account\Account;
 use App\Domain\Entities\Transaction\Transaction;
+use App\Domain\Exceptions\Transaction\InsufficientBalanceException;
 use App\Domain\Exceptions\Transaction\InvalidPayerAccountException;
 
 class TransactionValidatorService implements TransactionValidatorServiceInterface
 {
     public function validate(Transaction $transaction): void
     {
-        $this->validatePayerAccountType($transaction->getPayer());
+        $this->validatePayerAccountType($transaction);
+        $this->validatePayerAccountBalance($transaction);
     }
 
-    private function validatePayerAccountType(Account $payer): void
+    private function validatePayerAccountType(Transaction $transaction): void
     {
+        $payer = $transaction->getPayer();
         if($payer->isShopkeeper()) {
             throw new InvalidPayerAccountException();
+        }
+    }
+
+    private function validatePayerAccountBalance(Transaction $transaction): void
+    {
+        $payer = $transaction->getPayer();
+        if($payer->getBalance() < $transaction->getAmount()) {
+            throw new InsufficientBalanceException();
         }
     }
 }
--- tests/Unit/Feature/Transaction/Validator/TransactionValidatorServiceTest.php
+++ tests/Unit/Feature/Transaction/Validator/TransactionValidatorServiceTest.php
@@ -4,14 +4,16 @@ namespace Tests\Feature\Transaction\Validator;
 
 use App\Domain\Entities\Account\Account;
 use App\Domain\Entities\Account\Type\Shopkeeper;
+use App\Domain\Entities\Account\Type\Customer;
 use App\Domain\Entities\Transaction\Transaction;
+use App\Domain\Exceptions\Transaction\InsufficientBalanceException;
 use App\Domain\Exceptions\Transaction\InvalidPayerAccountException;
 use App\Domain\Services\Transaction\Validator\TransactionValidatorService;
 use Tests\TestCase;
 
 class TransactionValidatorServiceTest extends TestCase
 {
-    public function testInvalidatePayerAccountType()
+    public function testInvalidPayerAccountType()
     {
         $this->expectException(InvalidPayerAccountException::class);
 
@@ -20,6 +22,22 @@ class TransactionValidatorServiceTest extends TestCase
 
         $transaction = new Transaction();
         $transaction->setPayer($payer);
+
+        $validatorService = new TransactionValidatorService();
+        $validatorService->validate($transaction);
+    }
+
+    public function testPayerAccountInsufficientBalance()
+    {
+        $this->expectException(InsufficientBalanceException::class);
+
+        $payer = new Account();
+        $payer->setBalance(100);
+        $payer->setAccountType(new Customer());
+
+        $transaction = new Transaction();
+        $transaction->setPayer($payer);
+        $transaction->setAmount(200);
 
         $validatorService = new TransactionValidatorService();
         $validatorService->validate($transaction);
