e7069dac1a40abaf9a9ac7299778b4ef24fed57a
Fabio Andrei Lorentz <tifabio@gmail.com>
2022-09-12 10:37:47 -0300

    Update Service Test

--- tests/Feature/Transaction/TransactionServiceTest.php
+++ tests/Feature/Transaction/TransactionServiceTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Transaction;
 use App\Domain\Entities\Transaction\Transaction;
 use App\Domain\Services\Transaction\TransactionService;
 use App\Domain\Entities\Transaction\Status\Approved;
+use App\Domain\Entities\Transaction\Status\Finished;
 use App\Domain\Entities\Transaction\Status\Pending;
 use App\Infrastructure\ORM\Models\Account;
 use App\Infrastructure\ORM\Models\Transaction as TransactionModel;
@@ -14,12 +15,14 @@ use Tests\TestCase;
 class TransactionServiceTest extends TestCase
 {   
     private TransactionService $service;
+    private int $payerInitBalance;
 
     public function setUp(): void
     {
         parent::setUp();
 
         $this->service = app(TransactionService::class);
+        $this->payerInitBalance = 200;
     }
 
     public function testCreateTransaction()
@@ -32,8 +35,24 @@ class TransactionServiceTest extends TestCase
     public function testAuthorizeTransaction()
     {
         $transaction = $this->mockTransaction();
-        $authorizedTransaction = $this->service->authorize($transaction);
-        $this->assertTrue($authorizedTransaction->getTransactionStatus() instanceof Approved);
+        $authorizeTransaction = $this->service->authorize($transaction);
+        $this->assertTrue($authorizeTransaction->getTransactionStatus() instanceof Approved);
+    }
+
+    public function testUpdateTransaction()
+    {
+        $transaction = $this->mockTransaction();
+        $createTransaction = $this->service->create($transaction);
+        $transaction->setUid($createTransaction->id);
+        $authorizeTransaction = $this->service->authorize($transaction);
+        $updateTransaction = $this->service->update($authorizeTransaction);
+
+        $payer = Account::find($transaction->getPayer()->getUid())->getEntity();
+        $payee = Account::find($transaction->getPayee()->getUid())->getEntity();
+
+        $this->assertTrue($updateTransaction->getTransactionStatus() instanceof Finished);
+        $this->assertTrue($payer->getBalance() === $this->payerInitBalance - $transaction->getAmount());
+        $this->assertTrue($payee->getBalance() === $transaction->getAmount());
     }
 
     private function mockTransaction(): Transaction
@@ -46,7 +65,7 @@ class TransactionServiceTest extends TestCase
             'document' => $faker->cpf,
             'email' => $faker->email,
             'password' => password_hash($faker->password, PASSWORD_BCRYPT),
-            'balance' => 200,
+            'balance' => $this->payerInitBalance,
             'account_type' => 'CUSTOMER'
         ])->getEntity();
 
@@ -59,7 +78,7 @@ class TransactionServiceTest extends TestCase
             'account_type' => 'SHOPKEEPER'
         ])->getEntity();
 
-        $amount = $faker->numberBetween(50, 200);
+        $amount = $faker->numberBetween(50, $this->payerInitBalance);
 
         $transaction = new Transaction();
         $transaction->setPayer($payer);
